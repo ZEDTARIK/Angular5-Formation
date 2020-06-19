6077c1d9a376215907dfc0bc5b595fcc64eeefcd
zouhair <ettarak.zouhair@gmail.com>
2020-06-19 20:06:19 +0100

    add service to project and remove logic to service Employee

---
+++ src/app/employees/employee.service.ts
@@ -0,0 +1,50 @@
+import { Injectable } from '@angular/core';
+import { Employee } from '../models/employee.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmployeeService {
+
+  private listEmployee : Employee[] = [
+    {
+      id: 1,
+      name: 'Mark',
+      gender: 'Male',
+      contactPreference: 'Email',
+      email: 'mark@pragimtech.com',
+      dateOfBirth: new Date('10/25/1988'),
+      department: 'IT',
+      isActive: true,
+      photoPath: 'assets/Images/mark.png'
+    },
+    {
+      id: 2,
+      name: 'Mary',
+      gender: 'Female',
+      contactPreference: 'Phone',
+      phoneNumber: 2345978640,
+      dateOfBirth: new Date('11/20/1979'),
+      department: 'HR',
+      isActive: true,
+      photoPath: 'assets/Images/mary.png'
+    },
+    {
+      id: 3,
+      name: 'John',
+      gender: 'Male',
+      contactPreference: 'Phone',
+      phoneNumber: 5432978640,
+      dateOfBirth: new Date('3/25/1976'),
+      department: 'IT',
+      isActive: false,
+      photoPath: 'assets/Images/john.png'
+    },
+  ];
+
+  constructor() { }
+
+  getEmployees() : Employee[] {
+    return this.listEmployee;
+  }
+}
