923b6bc9a9ebf42e33540bb06b281d6482ecae9a
zouhair <ettarak.zouhair@gmail.com>
2020-06-21 01:41:10 +0100

    add Pagination next Employee

--- src/app/employees/display-employee.component.ts
+++ src/app/employees/display-employee.component.ts
@@ -14,5 +14,5 @@ export class DisplayEmployeeComponent implements OnInit {
 
   ngOnInit() {
   }
-
+  
 }
--- src/app/employees/list-employees.component.ts
+++ src/app/employees/list-employees.component.ts
@@ -10,11 +10,24 @@ import { EmployeeService } from './employee.service';
 export class ListEmployeesComponent implements OnInit {
 
   employees: Employee[] = [];
+  employeeToDispaly: Employee;
+  private arrayIndex = 1;
 
   constructor(private employeeService: EmployeeService) { }
 
   ngOnInit() {
     this.employees = this.employeeService.getEmployees();
+    this.employeeToDispaly = this.employees[0];
+  }
+
+  nextEmployee(): void {
+    if (this.arrayIndex < this.employees.length) {
+      this.employeeToDispaly = this.employees[this.arrayIndex];
+      this.arrayIndex++;
+    } else {
+      this.employeeToDispaly = this.employees[0];
+      this.arrayIndex = 1;
+    }
   }
 
 }
